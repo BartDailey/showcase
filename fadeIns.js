754ec92773deb6935a0a44f6beecb7ee03d709fa
Bart Dailey <bartdailey@gmail.com>
2016-02-15 16:24:11 -0700

    modified fadeIns.js

---
+++ fadeIns.js
@@ -0,0 +1,10 @@
+  $(document).ready(function(){
+            $("#bart").toggle("bounce");
+            $("#bart").animate({left:"200px"},"slow");
+            $("#open").fadeIn(10000);
+            $("#bart").animate({height:"150px",width:"150px"},2000);
+            $("#credit").fadeIn(7000);
+            
+          
+ 
+        });
